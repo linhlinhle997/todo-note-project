456a7936f858c2a6a6d8c9c551838829833cd70f
linhprimelabo <i.linh@primelabo.com.vn>
2022-04-18 13:08:39 +0700

    first

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,19 +1,8 @@
 from django.db import models
 from django.utils import timezone
 
-class Category(models.Model):
-    title = models.CharField(max_length=250, unique=True)
-    detail = models.TextField(null=True, blank=True)
-    created_date = models.DateField(default=timezone.now)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        ordering = ["-created_date"]
-
 class Todo(models.Model):
-    title = models.CharField(max_length=250)
+    title = models.CharField(max_length=250, unique=True)
     detail = models.TextField(null=True, blank=True)
     is_done = models.BooleanField(default=False)
     created_date = models.DateField(default=timezone.now)
@@ -24,3 +13,15 @@ class Todo(models.Model):
 
     def __str__(self):
         return self.title
+
+class Category(models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    detail = models.TextField(null=True, blank=True)
+    created_date = models.DateField(default=timezone.now)
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["-created_date"]
--- backend/todo/serializers.py
+++ backend/todo/serializers.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers
 from todo.models import Category, Todo
+from django.utils.text import slugify
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    slug = serializers.SerializerMethodField()
+
+    def get_slug(self, instance):
+        return slugify(instance.title)
+
     class Meta:
         model = Category
-        fields = "__all__"
+        fields = ("title", "detail", "created_date", "slug")
 
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    slug = serializers.SerializerMethodField()
+
+    def get_slug(self, instance):
+        return slugify(instance.title)
+
     class Meta:
         model = Todo
-        fields = "__all__"
+        fields = ("title", "detail", "is_done", "created_date", "due_date", "category", "slug")
